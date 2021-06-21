a0a4fbcaf12747ac94f0951b647690d804658695
Maksym Fedorenko <justmaksit@gmail.com>
2021-06-21 16:06:32 +0300

    Lesson 11 ngStyle dinamic background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title'
-  number = 42
-  obj = { a: 1, b: { c: 2 } }
-
-  inputValue = ''
-
-  // img = 'https://telegra.ph/file/ea145e37cbe1e95db8c14.png'
-
-  constructor() {
-    // setTimeout(() => {
-    //   console.log("over");
-    //   this.img = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg'
-    // }, 3000)
-  }
-
-  onInput(event: any) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
+  backgroundToggle: boolean = true
 
 }
